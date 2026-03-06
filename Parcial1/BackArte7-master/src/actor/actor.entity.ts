47e3ce821d397f4dfd73db96bf46a9e292390a41
Natalia Erazo <n.erazog@uniandes.edu.co>
2026-03-06 02:52:05 -0500

    arreglo

--- Parcial1/BackArte7-master/src/actor/actor.entity.ts
+++
@@ -1,9 +0,0 @@
-import { Entity, ManyToMany } from 'typeorm';
-import { MovieEntity } from '../movie/movie.entity';
-import { PersonEntity } from '../shared/entities/person.entity';
-
-@Entity()
-export class ActorEntity extends PersonEntity {
-    @ManyToMany(type => MovieEntity, movie => movie.actors)
-    movies: MovieEntity[];
-}
